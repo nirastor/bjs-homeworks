a387b3e7b66261f3da5438ecb61e90a812276d1d
nirastor <62954081+nirastor@users.noreply.github.com>
2020-06-18 20:01:58 +0300

    task 2-2 done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,39 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+    const arr = this.toLowerCase().split('').filter(i => i !== ' ');
+    const arrR = arr.slice().reverse();
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arrR[i]) return false;
+    }
+
+    return true;
 }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+function getAverageMark(marks) {
+    if (marks.length === 0) return 0;
+    return Math.round(marks.reduce((a, b) => a + b, 0) / marks.length);
+}
+
+function checkBirthday(date) {
+    const now = new Date();
+    const birthday = new Date(date);
+
+    // Если делать по тексту задания то будет как-то так
+    // const MS_IN_YEAR = 365.25 * 24 * 60 * 60 * 1000;
+    // return Math.floor((now - birthday)/MS_IN_YEAR) >= 18 ? true : false;
+
+    // Но это не совсем точно т.к. каждый 100-й год не вискокосный
+    // Также на 18 июня 2020 этот способ записывает в совершеннолетних тех кто родился 19 июня 2002
+    // Можно проверить сколько 29-х февраля было между датами рождения и проверки
+    // но кажетсяэто будет усложнение ради упрощения
+    
+    // пока что ничего реально точнее (и понятнее) вот этого придумать не могу
+    if ((now.getFullYear() - birthday.getFullYear()) > 18) return true;
+    if ((now.getFullYear() - birthday.getFullYear()) < 18) return false;
+
+    if (now.getMonth() > birthday.getMonth()) return true;
+    if (now.getMonth() < birthday.getMonth()) return false;
+
+    if (now.getDate() >= birthday.getDate()) return true;
+    if (now.getDate() < birthday.getDate()) return false;  
 }
