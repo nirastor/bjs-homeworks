8927236ab5793de1e1f9a12b2a10b698b1d27023
nirastor <62954081+nirastor@users.noreply.github.com>
2020-06-19 20:57:16 +0300

    task 2-2 done (add better arr compare)

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -2,11 +2,12 @@ String.prototype.isPalindrome = function() {
     const arr = this.toLowerCase().split('').filter(i => i !== ' ');
     const arrR = arr.slice().reverse();
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== arrR[i]) return false;
-    }
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] !== arrR[i]) return false;
+    // }
 
-    return true;
+    // return true;
+    return arr.join('') === arrR.join('');
 }
 
 function getAverageMark(marks) {
@@ -19,21 +20,22 @@ function checkBirthday(date) {
     const birthday = new Date(date);
 
     // Если делать по тексту задания то будет как-то так
-    // const MS_IN_YEAR = 365.25 * 24 * 60 * 60 * 1000;
-    // return Math.floor((now - birthday)/MS_IN_YEAR) >= 18 ? true : false;
+    const MS_IN_YEAR = 365.25 * 24 * 60 * 60 * 1000;
+    console.log((now - birthday)/MS_IN_YEAR);
+    return Math.floor((now - birthday)/MS_IN_YEAR) >= 18 ? true : false;
 
     // Но это не совсем точно т.к. каждый 100-й год не вискокосный
     // Также на 18 июня 2020 этот способ записывает в совершеннолетних тех кто родился 19 июня 2002
     // Можно проверить сколько 29-х февраля было между датами рождения и проверки
-    // но кажетсяэто будет усложнение ради упрощения
+    // но кажется это будет усложнение ради упрощения
     
     // пока что ничего реально точнее (и понятнее) вот этого придумать не могу
-    if ((now.getFullYear() - birthday.getFullYear()) > 18) return true;
-    if ((now.getFullYear() - birthday.getFullYear()) < 18) return false;
+    // if ((now.getFullYear() - birthday.getFullYear()) > 18) return true;
+    // if ((now.getFullYear() - birthday.getFullYear()) < 18) return false;
 
-    if (now.getMonth() > birthday.getMonth()) return true;
-    if (now.getMonth() < birthday.getMonth()) return false;
+    // if (now.getMonth() > birthday.getMonth()) return true;
+    // if (now.getMonth() < birthday.getMonth()) return false;
 
-    if (now.getDate() >= birthday.getDate()) return true;
-    if (now.getDate() < birthday.getDate()) return false;  
+    // if (now.getDate() >= birthday.getDate()) return true;
+    // if (now.getDate() < birthday.getDate()) return false;  
 }
